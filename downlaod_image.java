2861b304b45c6125522fc870486d22dfc9c04084
Syed Athuallah R <syedathaullah786786@gmail.com>
2025-08-02 23:26:14 +0530

    Update downlaod_image.java

--- downlaod_image.java
+++ downlaod_image.java
@@ -18,10 +18,12 @@ public class downlaod_image {
             inputStream.close();
             fos.close();
             System.out.println("Image downloaded successfully "+fileLocation);
-        }    
+        }  
+            
         else{
             System.out.println("Image download failed");
         } 
         conn.disconnect();
     }
 }
+

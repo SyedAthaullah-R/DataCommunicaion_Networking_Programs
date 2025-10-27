98d4672f5eed9d68d6448df420d4c63c8d26d1ce
Syed Athuallah R <syedathaullah786786@gmail.com>
2025-10-27 23:54:53 +0530

    Fix formatting issues in downlaod_image.java

--- downlaod_image.java
+++ downlaod_image.java
@@ -19,13 +19,13 @@ public class downlaod_image {
             fos.close();
             System.out.println("Image downloaded successfully "+fileLocation);
         }   
-            
         else{
             System.out.println("Image download failed");
         } 
         conn.disconnect();
     }
 }
+
 
 
 
